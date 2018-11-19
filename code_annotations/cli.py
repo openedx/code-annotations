fb522733107c8f4a92b59ca279bea46e3cb6d8d0
Troy Sankey <tsankey@edx.org>
2018-11-19 17:05:58 -0500

    Add django reporting module and safelist seeder
    PLAT-2356

--- code_annotations/cli.py
+++ code_annotations/cli.py
@@ -1,20 +1,74 @@
 """
 Command line interface for code annotation tools.
 """
+import os
+import sys
+
 import click
+import yaml
+from code_annotations.django_reporting_helpers import get_models_requiring_annotations
+
+DEFAULT_SAFELIST_FILE_PATH = '.pii_safe_list.yaml'
+
+
+def fail(msg):
+    """
+    Log the message and exit.
+    """
+    click.echo(msg)
+    sys.exit(1)
 
 
 @click.group()
-def cli():
+@click.option(
+    '--config_file', 'config_file_path',
+    type=click.Path(exists=True, dir_okay=False),
+    help='Config file for all code annotation tools.',
+)
+@click.pass_context
+def cli(ctx, config_file_path):
     """
     Code annotation tools.
     """
-    pass
+    # Code placed in this function runs first, before entering any subcommands.
+    ctx.ensure_object(dict)
+    with open(config_file_path) as config_file:
+        ctx.obj['config'] = yaml.load(config_file)
+    # Now, the ctx object will be passed along to subcommands as the first argument.
 
 
-@cli.command('seed_safelist')
-def seed_safelist():
+@cli.command('pii_report_django')
+@click.option(
+    '--seed_safelist',
+    is_flag=True,
+    help='Generate an initial safelist file based on the current Django environment.',
+)
+@click.pass_context
+def pii_report_django(ctx, seed_safelist):
     """
-    Subcommand for seeding the initial .pii_safe_list.yaml file.
+    Subcommand for dealing with PII in Django models.
     """
-    pass
+    safelist_file_path = ctx.obj['config'].get('safelist_path', DEFAULT_SAFELIST_FILE_PATH)
+    if seed_safelist:
+        if os.path.exists(safelist_file_path):
+            fail('{} already exists, not overwriting.'.format(safelist_file_path))
+        local_model_ids, non_local_model_ids = get_models_requiring_annotations()
+        click.echo(
+            'Listing {} local models requiring annotations:'.format(len(local_model_ids))
+        )
+        for model_id in local_model_ids:
+            click.echo('     {}'.format(model_id))
+        click.echo(
+            'Found {} non-local models requiring annotations. Adding them to safelist.'.format(len(non_local_model_ids))
+        )
+        click.echo(
+            'Found {} local models requiring annotations. NOT adding them to safelist.'.format(len(local_model_ids))
+        )
+        safelist_data = {model_id: {} for model_id in non_local_model_ids}
+        with open(safelist_file_path, 'w') as safelist_file:
+            yaml.dump(safelist_data, stream=safelist_file)
+        click.echo('Successfully created safelist file "{}".'.format(safelist_file_path))
+        click.echo('Now, you need to:')
+        click.echo('  1) Make sure that any un-annotated models in the safelist are annotated, and')
+        click.echo('  2) Annotate the local models listed above.')
+        return  # this was a special function of the pii_report_django subcommand, so terminate the program here.
--- tests/test_code_annotations.py
+++ tests/test_code_annotations.py
@@ -1,17 +1,83 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Tests for the `code-annotations` models module.
+Tests for the `code-annotations` module.
 """
 from __future__ import absolute_import, unicode_literals
 
-from code_annotations import cli
+import os
+
+from click.testing import CliRunner
+from code_annotations.cli import cli
+from mock import DEFAULT, patch
+
+FAKE_SAFELIST_PATH = 'fake_safelist_path.yaml'
+
+FAKE_CONFIG_FILE = """
+safelist_path: {fake_safelist_path}
+""".format(
+    fake_safelist_path=FAKE_SAFELIST_PATH
+)
 
 
-def test_dummy():
+def _call_script(args_list, test_filesystem_cb=None):
     """
-    Dummy test
+    Call the code_annotations script with the given params and a generic config file.
 
-    Delete this test after the first real test is added.
+    Returns:
+        click.testing.Result: Result from the `CliRunner.invoke()` call.
     """
-    assert hasattr(cli, '__doc__')
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open('test_config.yml', 'w') as f:
+            f.write(FAKE_CONFIG_FILE)
+
+        result = runner.invoke(
+            cli,
+            args=[
+                '--config_file', 'test_config.yml',
+            ] + args_list
+        )
+        if test_filesystem_cb:
+            test_filesystem_cb()
+    print(result)
+    print(result.output)
+    return result
+
+
+@patch.multiple(
+    'code_annotations.cli',
+    get_models_requiring_annotations=DEFAULT,
+)
+def test_seeding_safelist(**kwargs):
+    """
+    """
+    mock_get_models_requiring_annotations = kwargs['get_models_requiring_annotations']
+    local_model_ids = [
+        'fake_app_1.FakeModelA',
+        'fake_app_2.FakeModelB',
+    ]
+    non_local_model_ids = [
+        'fake_app_3.FakeModelC',
+        'fake_app_4.FakeModelD',
+    ]
+    mock_get_models_requiring_annotations.return_value = (
+        local_model_ids,
+        non_local_model_ids,
+    )
+
+    def test_safelist_callback():
+        assert os.path.exists(FAKE_SAFELIST_PATH)
+        with open(FAKE_SAFELIST_PATH, 'r') as fake_safelist_file:
+            fake_safelist = fake_safelist_file.read()
+        for model_id in non_local_model_ids:
+            assert model_id in fake_safelist
+        for model_id in local_model_ids:
+            assert model_id not in fake_safelist
+
+    result = _call_script(
+        ['pii_report_django', '--seed_safelist'],
+        test_filesystem_cb=test_safelist_callback,
+    )
+    assert result.exit_code == 0
+    assert 'Successfully created safelist file' in result.output
