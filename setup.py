fb13058f20013c0468880584b63ccfb71ba4302d
Jawayria <39649635+Jawayria@users.noreply.github.com>
2022-07-23 20:04:06 +0500

    fix: Install pip and pip-tools in upgrade script (#81)

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
     version=VERSION,
     description="""Extensible tools for parsing annotations in codebases""",
     long_description=README + '\n\n' + CHANGELOG,
+    long_description_content_type='text/x-rst',
     author='edX',
     author_email='oscm@edx.org',
     url='https://github.com/edx/code-annotations',
