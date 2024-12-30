934d1551f7f64a2c1e248e29acba46444acacfc7
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-12-30 10:36:34 +0100

    fix: replace with importlib.resources

--- code_annotations/contrib/config/__init__.py
+++ code_annotations/contrib/config/__init__.py
@@ -3,13 +3,13 @@ Expose contrib configuration file paths as Python variables, for use in 3rd-part
 """
 import os
 
-import importlib_resources
+import importlib.resources
 
-FEATURE_TOGGLE_ANNOTATIONS_CONFIG_PATH = importlib_resources.files(
+FEATURE_TOGGLE_ANNOTATIONS_CONFIG_PATH = importlib.resources.files(
     "code_annotations") / os.path.join("contrib", "config", "feature_toggle_annotations.yaml")
 
-SETTING_ANNOTATIONS_CONFIG_PATH = importlib_resources.files(
+SETTING_ANNOTATIONS_CONFIG_PATH = importlib.resources.files(
     "code_annotations") / os.path.join("contrib", "config", "setting_annotations.yaml")
 
-OPENEDX_EVENTS_ANNOTATIONS_CONFIG_PATH = importlib_resources.files(
+OPENEDX_EVENTS_ANNOTATIONS_CONFIG_PATH = importlib.resources.files(
     "code_annotations") / os.path.join("contrib", "config", "openedx_events_annotations.yaml")
